178166149c2ca15867dd53907dcc026267ca8540
lele <2432371796@qq.com>
2022-05-27 15:08:53 +0800

    调整checkpoint

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ Vocab()
 if __name__ == '__main__':
     # 超参
     # num_epoch batch_size max_word embedding_dim hidden_size num_layers 双向 dropout
-    num_epoch_lstm = 5
+    # num_epoch_lstm = 5
     # num_epoch_gru = 5
-    batch_size = 100
     # num_epoch_rnn = 4
+    batch_size = 100
     # batch_size_rnn = 50
     train_iter, imdb_ds = get_loader(batch_size=batch_size, pattern='train')
     # 训练
-    checkpoint_path = "./net/rnn_checkpoint.pkl"
+    checkpoint_path = "./net/gru_checkpoint.pkl"
     # 两层rnn方便加dropout
     # rnn_layer = nn.RNN(input_size=200, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True, dropout=0.5)
-    # rnn_layer = nn.GRU(input_size=200, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True, dropout=0.5)
-    rnn_layer = nn.LSTM(input_size=200, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True, dropout=0.5)
+    rnn_layer = nn.GRU(input_size=200, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True, dropout=0.5)
+    # rnn_layer = nn.LSTM(input_size=200, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True, dropout=0.5)
     net = net.rnn_net(rnn_layer=rnn_layer, vocab_size=imdb_ds.get_vocab_size(), embedding_dim=200).to(device)
-    # train_eval.train(net, train_iter, num_epoch=num_epoch_lstm, checkpoint_path=checkpoint_path, device=device)
+    # train_eval.train(net, train_iter, num_epoch=num_epoch_rnn, checkpoint_path=checkpoint_path, device=device)
 
     net.load_state_dict(torch.load(checkpoint_path)['model_state_dict'])
     loss, accuracy = train_eval.eval(net, 'test', device)
